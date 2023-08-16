60feafeeba22b91f113e61b263c8e5c2a430b414
Ayman-ing <aymanfeki1@gmail.com>
2023-08-16 01:35:06 +0100

    making the sport in list

--- app.py
+++ app.py
@@ -138,17 +138,19 @@ def logout():
     return redirect("/")
 @app.route("/members", methods=["GET", "POST"])
 def members():
-    #affiching the current members
+    #getting necessary informations 
     clients=db.execute("select cl_Fname,cl_Lname,cl_num,cl_BD,student from client;")
+    sport=db.execute("select sp_name from sport;")
+    membership=db.execute("select m_id,cl_id,sp_id,ends_at from membership;")
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         #checking the validty of the phone number
         if (not request.form.get("cl_num").isnumeric()) or len(request.form.get("cl_num"))<8:
-            return render_template('members.html', message1="phone number invalid",clients=clients)
+            return render_template('members.html', message1="phone number invalid",clients=clients,sport=sport,membership=membership)
         #checking the phone number already exist
         number=db.execute("select cl_Fname from client where cl_num=?;",request.form.get("cl_num"))
         if len(number)>0:
-            return render_template('members.html', message3="phone number already exist",clients=clients)
+            return render_template('members.html', message3="phone number already exist",clients=clients,sport=sport,membership=membership)
         #adding the member to the table
         db.execute(
                 "insert into client (cl_Fname,cl_Lname,cl_num,cl_BD,student) values(?,?,?,?,?)",
@@ -156,15 +158,32 @@ def members():
                 request.form.get("cl_Lname"),
                 int(request.form.get("cl_num")),
                 request.form.get("cl_BD"),
-                request.form.get("student")
+                request.form.get("student") 
 
             )
         clients=db.execute("select cl_Fname,cl_Lname,cl_num,cl_BD,student from client;")
 
+        if (not request.form.get("sp_name")) and (not request.form.get("m_type"))  and  (not request.form.get("duration")) and (not request.form.get("price")) and (not request.form.get("starts_at")):    
+            return render_template('members.html',message2="member added successfuly",clients=clients,sport=sport,membership=membership)
+        if (not request.form.get("sp_name")) or (not request.form.get("m_type"))  or  (not request.form.get("duration")) or (not request.form.get("price")) or (not request.form.get("starts_at")):
+            return render_template('members.html',message6="member added successfuly ",message4="No membership added ! Not enough informations",clients=clients,sport=sport,membership=membership)
+        cl_id=db.execute("select cl_id from client where cl_num=?",int(request.form.get("cl_num")))
+        sp_id=db.execute("select sp_id from sport where sp_name=?",request.form.get("sp_name"))
+        db.execute(
+                "insert into membership (cl_id,sp_id,m_type,duration,price,starts_at,ends_at,m_status,benefits) values(?,?,?,?,?,?,date(?,'+' || ? || ' months'),?,?)",
+                cl_id[0]["cl_id"],
+                sp_id[0]["sp_id"],
+                request.form.get("m_type"),
+                int(request.form.get("duration")),
+                int(request.form.get("price")),
+                request.form.get("starts_at"),
+                request.form.get("starts_at"),
+                int(request.form.get("duration")),
+                'active','NULL'
+                )
+        return render_template('members.html',message5="member and his membership added successfuly",clients=clients,sport=sport,membership=membership)
         
-        return render_template('members.html',message2="member added successfuly",clients=clients)
         
-        
-        
-    else:    
-        return render_template('members.html',clients=clients)
+    else:
+            
+        return render_template('members.html',clients=clients,sport=sport,membership=membership)
